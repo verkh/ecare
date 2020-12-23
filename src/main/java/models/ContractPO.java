517e964d7c03b0f9722537eec9cd2e8e49d1c79c
Denis Verkhovskii <denis.verkhovskii@t-systems.com>
2020-12-23 13:54:39 +0300

    implement ContractPO

--- src/main/java/models/ContractPO.java
+++ src/main/java/models/ContractPO.java
@@ -2,12 +2,31 @@ package models;
 
 import models.base.AbstractNamedPO;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table (name = "contracts")
 public class ContractPO extends AbstractNamedPO {
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
+    @OneToOne
+    @JoinColumn(name = "plan_id", referencedColumnName = "id")
+    private PlanPO plan;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public PlanPO getPlan() {
+        return plan;
+    }
+
+    public void setPlan(PlanPO plan) {
+        this.plan = plan;
+    }
 }
